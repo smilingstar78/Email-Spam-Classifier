9f21231af606899b90137dde6fee44b669ce9a9c
Muskan Tariq <155077202+smilingstar78@users.noreply.github.com>
2025-07-29 03:08:22 -0700

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import streamlit as st
 import pickle
 import nltk
 nltk.download('punkt')
+nltk.download('punkt_tab')
+nltk.download('stopwords')
 from nltk.corpus import stopwords
 import string
 from nltk.stem.porter import PorterStemmer
